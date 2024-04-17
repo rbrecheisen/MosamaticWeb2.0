6299186982aff549ae6d310e0d7b2e8ab0beab0e
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-04-17 16:38:13 +0200

    Saving work

--- src/mosamaticweb/app/management/commands/initialize_tasks.py
+++ src/mosamaticweb/app/management/commands/initialize_tasks.py
@@ -1,8 +1,31 @@
 from django.core.management.base import BaseCommand
 
+from ...models.taskmodel import TaskModel
+
 
 class Command(BaseCommand):
     help = 'Initialize tasks'
 
     def handle(self, *args, **kwargs):
+        tasks = [
+            TaskModel.objects.create(
+                name='dummy',
+                parameters=[
+                    {
+                        'name': 'model_files',
+                        'display_name': 'AI model files',
+                        'data_type': 'dataset',
+                        'required': True,
+                    },
+                    {
+                        'name': 'l3_images',
+                        'display_name': 'L3 images',
+                        'data_type': 'dataset',
+                        'required': True
+                    },
+                ]
+            )
+        ]
+        for task in tasks:
+            self.stdout.write(self.style.SUCCESS(f' > Created task {task.name}'))
         self.stdout.write(self.style.SUCCESS('Successfully initialized tasks'))
