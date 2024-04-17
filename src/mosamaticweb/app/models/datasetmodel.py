1033fb08a34926a029c897be58796bb803131e4d
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-04-17 20:09:10 +0200

    Saving work

--- src/mosamaticweb/app/models/datasetmodel.py
+++ src/mosamaticweb/app/models/datasetmodel.py
@@ -8,5 +8,5 @@ class DatasetModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=1024, unique=True)
     owner = models.OneToOneField(settings.AUTH_USER_MODEL)
-    origin = models.CharField(max_length=1024, unique=False)
+    data_dir = models.CharField(max_length=2048, unique=False)
     files = models.ManyToManyField('FileModel', related_name='dataset')
